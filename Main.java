427e1481ce12f173c5a7488c7a70ccea2d9de1a9
mugi-uno <mugi.uno@gmail.com>
2017-02-24 11:37:41 +0900

    生存・過疎・過密を実装
    初期状態は wikipedia "ライフゲーム" 記載の「ブリンカー」を参考にした
    (参考: https://ja.wikipedia.org/wiki/%E3%83%A9%E3%82%A4%E3%83%95%E3%82%B2%E3%83%BC%E3%83%A0)

--- Main.java
+++ Main.java
@@ -7,6 +7,9 @@ public class Main {
 				b[i][j] = "□";
 			}
 		}
+		b[2][1] = "■";
+		b[2][2] = "■";
+		b[2][3] = "■";
 
 		for (int i = 0; i < 5; i++) {
 			String line = "";
@@ -17,6 +20,36 @@ public class Main {
 		}
 
 		for (int times = 0; times < 3; times++) {
+			String[][] bb = new String[5][5];
+
+			for (int i = 0; i < 5; i++) {
+				bb[i] = new String[5];
+
+				for (int j = 0; j < 5; j++) {
+					int count = 0;
+
+					bb[i][j] = b[i][j];
+
+					if (b[i][j].equals("■")) {
+						// 生存・過疎・過密の場合
+						count = 0;
+
+						if (i > 0 && b[i - 1][j].equals("■")) count += 1;
+						if (j > 0 && b[i][j - 1].equals("■")) count += 1;
+						if (j < 4 && b[i][j + 1].equals("■")) count += 1;
+						if (i < 4 && b[i + 1][j].equals("■")) count += 1;
+
+						if (count == 2) {
+							bb[i][j] = b[i][j];
+						} else {
+							bb[i][j] = "□";
+						}
+					}
+				}
+			}
+
+			b = bb;
+
 			System.out.println(times + "==========");
 
 			for (int i = 0; i < 5; i++) {
